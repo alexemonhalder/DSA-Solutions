efe74c5c9b17904b4665f0d234a339d11cf77661
Alex Emon Halder <100570799+alexemonhalder@users.noreply.github.com>
2025-09-17 19:07:44 +0600

    added solution for Leetcode 1413 (prefix sum)

---
+++ arrays/1413_minimum_value_to_get_step_by_step_sum_prefix_sum.java
@@ -0,0 +1,30 @@
+/* 
+Given an integer array nums, find the minimum positive integer startValue such that 
+the running sum of startValue plus elements of nums (from left to right) never drops below 1.
+
+Problem: Leetcode- 1413. Minimum Value to Get Positive Step by Step Sum
+Approach: Prefix sum
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+    public int minStartValue(int[] nums) {
+
+        int []prefix=new int[nums.length];
+        int minPrefix=0;
+
+        prefix[0]=nums[0];
+        for(int i=1; i<nums.length; i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+
+        for(int i=0; i<nums.length; i++){
+            if(prefix[i]<minPrefix){
+                minPrefix=prefix[i];
+            }
+        }
+
+        return 1-minPrefix;
+    }
+}
